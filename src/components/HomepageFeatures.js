6a43e2188e2a6bc0cc9d433dad58e1593c02df73
irustm <jamakrus@gmail.com>
2021-08-16 19:50:51 +0300

    Fix i18n pages

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -8,14 +8,36 @@ export default function HomepageFeatures() {
     <section className={styles.features}>
       <div className="container">
         <div>
-          <h3>Features</h3>
+          <h3>
+            <Translate
+            id="homepage.titleFeatures">
+            Features
+            </Translate>
+          </h3>
           <ul>
-            <li> <b>Area</b> - these are the modules of your application.</li> 
-            <li> <b>Controller</b> - are responsible for controlling the flow of the application execution </li> 
-            <li> <b>Hooks</b>  - middleware for area, controller and actions with support DI. Have 3 life cyclic functions: <code>onPreAction, onPostAction, onCatchAction</code> </li>
-            <li> <b>Decorators</b> - for query, cookie, parametrs, routes and etc. </li> 
-            <li> <b>Dependency Injection</b> - for all controllers and hooks by default from microsoft/TSyringe (more about alosaur injection). </li> 
-            <li>  Render pages any template render engine. (more)</li> 
+          
+
+            <li> <b>Area</b>
+              <Translate id="homepage.futures.Area"> - these are the modules of your application. </Translate>
+             </li> 
+            <li> <b>Controller</b>
+              <Translate id="homepage.futures.Controller">  - are responsible for controlling the flow of the application execution </Translate>
+            </li> 
+            <li> <b>Hooks</b> 
+            <Translate id="homepage.futures.Hooks"> - middleware for area, controller and actions with support DI. Have 3 life cyclic functions:</Translate>
+              <code>onPreAction, onPostAction, onCatchAction</code>
+            </li>
+            <li> <b>Decorators</b>
+            <Translate id="homepage.futures.Decorators"> - for query, cookie, parametrs, routes and etc. </Translate>
+             </li> 
+            <li> <b>Dependency Injection</b>
+            <Translate id="homepage.futures.DI"> - for all controllers and hooks by default from microsoft/TSyringe </Translate>
+             </li> 
+            <li>
+            <Translate id="homepage.futures.Other">
+              Render pages any template render engine.
+              </Translate>
+            </li> 
           </ul>
         </div>
       </div>
